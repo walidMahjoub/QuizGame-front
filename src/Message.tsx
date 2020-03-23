02c35453210b37eaf176ddbe61e10973a1ced5ac
walidMahjoub <walid.kouissah.mahjoub@gmail.com>
2020-03-23 14:04:23 +0100

    setup eslint prettier

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react'
- 
+import React from 'react'
+
 class Message extends React.Component<MessageProps> {
-  render() {
-    return (
-      <div>
-        {`You got a new message from ${this.props.from}: ${this.props.body}`}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                {`You got a new message from ${this.props.from}: ${this.props.body}`}
+            </div>
+        )
+    }
 }
- 
+
 interface MessageProps {
-  body: string
-  from: string
+    body: string
+    from: string
 }
- 
+
 export default Message
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Message from "./Message";
-import App from "./App";
+import Message from "./Message"
+import AppJs from './App'
+import HooksTs from './HooksTs';
 
 ReactDOM.render(
-  <Router>
-    <Route
-      exact
-      path="/"
-      component={() => <Message from="Max" body="Hi !" />}
-    />
-    <Route exact path="/app" component={App} />
-  </Router>,
-  document.getElementById("root")
+    <Router>
+        <Route
+            exact
+            path="/"
+            component={() => <Message from="Max" body="Hi !" />}
+        />
+        <Route exact path="/app" component={AppJs} />
+        <Route exact path="/hooksTs" component={HooksTs} />
+    </Router>,
+    document.getElementById('root')
 );
